b373ff392ae32b1a0b0b7686df1827de537e60ac
cory meyers <whippoorwill.in.flight@gmail.com>
2016-01-18 15:17:12 -0800

    add function for years dividied by 100

--- spec/scripts.js
+++ spec/scripts.js
@@ -1,3 +1,9 @@
-function leapYear(year) {
-  return false;
-}
+var leapYear = function(year) {
+  if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+  return true;
+  } else {
+    return false;
+  }
+};
