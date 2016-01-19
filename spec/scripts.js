ec988e3051c75dbc0fc3d805391a71e27e586fa9
cory meyers <whippoorwill.in.flight@gmail.com>
2016-01-18 16:58:29 -0800

    add final code from lesson

--- spec/scripts.js
+++ spec/scripts.js
@@ -1,9 +1,39 @@
-var leapYear = function(year) {
-  if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) {
-  return true;
+//var leapYear = function(year) {
+//  if (year % 100 === 0) {
+//    return false;
+//  } else if (year % 4 === 0) {
+//  return true;
+//  } else {
+//    return false;
+//  }
+//};
+
+//function leapYear(year) {
+//  return ((year % 4 ===0) && (year % 100 !==100)) || (year % 400 ===0);
+//}
+
+function leapYear(year) {
+  if ((year % 4 ===0) && (year % 100 !==100) || (year % 400 ===0)){
+    return true;
   } else {
     return false;
   }
 };
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+
+    if (!result) {                 // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
